4bad40509a3d98f36fa81e24c443f3aa5b1c5fb1
Nonolar <165144280+Nonolar@users.noreply.github.com>
2024-11-04 14:03:03 +0100

    Update Program.cs
    Added some simple algorithms that are meant to get a  interest rate based on some simple parameters

--- CsharpStarterkit/Program.cs
+++ CsharpStarterkit/Program.cs
@@ -1,4 +1,4 @@
-﻿using CsharpStarterkit;
+using CsharpStarterkit;
 using Newtonsoft.Json;
 using System.Text;
 
@@ -19,14 +19,99 @@ GameInput input = new()
 string mapDataText = File.ReadAllText(mapFile);
 MapData mapData = JsonConvert.DeserializeObject<MapData>(mapDataText);
 
+//Hard coded personality values
+personality[] personality = new personality[5]
+{
+    new()
+    {
+        name = "conservative",
+        happinesMultiplier = 0.2m,
+        acceptedMinInterest = 0.0m,
+        acceptedMaxInterest = 0.02m,
+        livingStandardMultiplier = 0.1m
+    },
+    new()
+    {
+        name = "riskTaker",
+        happinesMultiplier = 0.2m,
+        acceptedMinInterest = 0.1m,
+        acceptedMaxInterest = 100000m,
+        livingStandardMultiplier = 0.1m
+    },
+    new()
+    {
+        name = "innovative",
+        happinesMultiplier = 0.2m,
+        acceptedMinInterest = 0.02m,
+        acceptedMaxInterest = 0.1m,
+        livingStandardMultiplier = 0.1m
+    },
+    new()
+    {
+        name = "practical",
+        happinesMultiplier = 0.2m,
+        acceptedMinInterest = 0.01m,
+        acceptedMaxInterest = 0.05m,
+        livingStandardMultiplier = 0.1m
+    },
+    new()
+    {
+        name = "spontaneous",
+        happinesMultiplier = 0.2m,
+        acceptedMinInterest = 0.01m,
+        acceptedMaxInterest = 0.5m,
+        livingStandardMultiplier = 0.1m
+    }
+};
+
+List<Customer> activeCustomers = new List<Customer>();
+
 foreach (Customer customer in mapData.customers)
 {
-    input.Proposals.Add(new CustomerLoanRequestProposal()
+    //The basic interestRate
+    decimal interestRate = personality.ToList().Find(n => n.name.ToLower() == customer.personality.ToLower()).acceptedMaxInterest/2;
+
+    //If interest is over 100% set to 50%
+    if(interestRate > 1)
     {
-        CustomerName = customer.name,
-        MonthsToPayBackLoan = mapData.gameLengthInMonths,
-        YearlyInterestRate = 0.1m,
-    });
+        interestRate = 0.5m;
+    }
+
+    //Add a part of the income to the interest rate
+    interestRate += customer.income * 0.0000001m;
+
+    //If loan is beneficial for the environment do a discount
+    if(customer.loan.environmentalImpact < 50)
+    {
+        interestRate -= 0.01m;
+    }
+
+    //If student loan do a discount proportional to their income
+    if (customer.hasStudentLoan)
+    {
+        interestRate -= customer.income * 0.0000002m;
+    }
+
+    //If person doesn't have the money to pay back the loan with the specified interest rate deny the loan 
+    //
+    if (customer.capital + customer.income * mapData.gameLengthInMonths - (decimal)Math.Pow((double)interestRate, mapData.gameLengthInMonths) * customer.loan.amount < 1)
+    {
+        interestRate = decimal.MaxValue;
+    }
+    else
+    {
+        activeCustomers.Add(customer);
+        input.Proposals.Add(new CustomerLoanRequestProposal()
+        {
+            CustomerName = customer.name,
+            MonthsToPayBackLoan = mapData.gameLengthInMonths,
+            YearlyInterestRate = interestRate,
+        });
+    }
+
+    
+
+    
 }
 
 Random random = new Random();
@@ -44,10 +129,25 @@ for (int i = 0; i < mapData.gameLengthInMonths; i++)
     //input.Iterations.Add(x);
     Dictionary<string, CustomerAction> t = new();
     input.Iterations.Add(t);
-    foreach (var customer in mapData.customers)
+    foreach (var customer in activeCustomers)
     {
+        //Award good people here
+
         string randomType = actionTypes[random.Next(actionTypes.Length)];
         string randomAward = randomType == "Skip" ? "None" : awardTypes[random.Next(awardTypes.Length)];
+
+        if (customer.loan.environmentalImpact < 50)
+        {
+            randomAward = random.Next(0, 2) == 1 ? "NoInterestRate" : "HalfInterestRate";
+        }
+        else
+        {
+            randomAward = "GiftCard";
+        }
+
+
+
+        
         t.Add(customer.name, new CustomerAction
         {
             Type = randomType,
@@ -65,3 +165,13 @@ request.Content = new StringContent(JsonConvert.SerializeObject(input), Encoding
 var res = client.Send(request);
 Console.WriteLine(res.StatusCode);
 Console.WriteLine(await res.Content.ReadAsStringAsync());
+
+
+class personality
+{
+    public string name;
+    public decimal happinesMultiplier { get; set; }
+    public decimal acceptedMinInterest { get; set; }
+    public decimal acceptedMaxInterest { get; set; }
+    public decimal livingStandardMultiplier {  get; set; }
+}
